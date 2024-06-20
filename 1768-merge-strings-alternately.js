b0fd998ab1a7457b8e3966c9ce8ff234a7d3592d
heegenie <heegenie.dv@gmail.com>
2024-06-20 23:12:58 +0900

    refactor: implement two pointers approach for leetcode solution 1768

--- 1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately.js
@@ -15,3 +15,27 @@ var mergeAlternately = function(word1, word2) {
 
   return arr.join('');
 };
+
+
+/**
+ * using two pointers
+ */
+var mergeAlternately = function(word1, word2) {
+  let result = '';
+  let i = 0;
+  let j = 0;
+
+  while (i < word1.length || j < word.length) {
+    if (i < word1.length) {
+      result += word1[i];
+      i++;
+    }
+
+    if (j < word2.length) {
+      result += word2[j];
+      j++;
+    }
+  }
+
+  return result;
+}
